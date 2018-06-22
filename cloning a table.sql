11a0e6853e172dcf474af81d524826f9f16f591b
MONISHA68 <33077811+MONISHA68@users.noreply.github.com>
2018-06-22 11:41:25 +0530

    Update cloning a table.sql

--- cloning a table.sql
+++ cloning a table.sql
@@ -6,3 +6,16 @@ select *from name;
 create table newname like name;
 insert newname select *from name;
 select *from newname;
+
+
+
+
+
+////simple cloning
+create table name(name varchar(15),salary int,gender varchar(2));
+desc name;
+insert into name values('moni',1500,'f');
+insert into name values('abi',2000,'f');
+select *from name;
+create table newname select *from name;
+
